540c3e5462691591d77f82de2d23a3c4ca7cb9f0
VornicuCatalina <catalina.tiger25@gmail.com>
2023-05-27 15:25:02 +0300

    wrong db

--- Project/src/main/java/org/project/databases/Database.java
+++ Project/src/main/java/org/project/databases/Database.java
@@ -7,7 +7,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class Database {
-    private static final String URL = "jdbc:postgresql://localhost:5432/lab8";
+    private static final String URL = "jdbc:postgresql://localhost:5432/project";
     private static final String USER = "postgres";
     private static final String PASSWORD = "admin123";
     private static Connection connection = null;
